ef1053d15955c02161edb5ba91a5b2af36be847b
Fernando <fernandoventurini9@hotmail.com>
2026-01-26 18:41:55 +0000

    coreModules #26

---
+++ 26-coreModules/index.js
@@ -0,0 +1,5 @@
+const path = require('path'); // Importa o módulo path do Node.js
+
+const extension = path.extname('arquivo.txt'); // Usa a função extname do módulo path
+
+console.log(extension); // Imprime a extensão do arquivo (".txt")
