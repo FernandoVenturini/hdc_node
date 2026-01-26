f9b82cb898df037fd8a07dc511e262fb17832265
Fernando <fernandoventurini9@hotmail.com>
2026-01-26 18:18:17 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const meuModulo = require('./modulo'); // Importa o módulo local
+const soma = meuModulo.soma; // Usa a função soma do módulo importado
+
+soma(3, 5); // Chama a função soma com argumentos 3 e 5
---
+++ modulo.js
@@ -0,0 +1,7 @@
+// MODULOs INTERNOS
+
+module.exports = {
+	soma(a, b) {
+		return a + b;
+	}
+}
