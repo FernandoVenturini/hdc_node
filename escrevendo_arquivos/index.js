97bb241de2b57e90465012d4512f313400050bd8
Fernando <fernandoventurini9@hotmail.com>
2026-04-04 11:08:31 +0100

    writing files #50b

--- escrevendo_arquivos/index.js
+++ escrevendo_arquivos/index.js
@@ -1,22 +1,34 @@
-const http = require('http'); // Importa o módulo HTTP para criar um servidor
-const fs = require('fs'); // Importa o módulo FS para manipular arquivos
-const url = require('url'); // Importa o módulo URL para analisar URLs
+const http = require("http"); // Importa o módulo HTTP para criar um servidor
+const fs = require("fs"); // Importa o módulo FS para manipular arquivos
+const url = require("url"); // Importa o módulo URL para analisar URLs
 
 const port = 3000; // Define a porta onde o servidor irá escutar
 
 // Cria um servidor HTTP
 const server = http.createServer((req, res) => {
-    const urlInfo = require('url').parse(req.url, true); // Analisa a URL da requisição
+    const urlInfo = require("url").parse(req.url, true); // Analisa a URL da requisição
     const name = urlInfo.query.name; // Obtém o valor do parâmetro 'name' da URL
 
-    fs.readFile('index.html', 'utf8', (err, data) => { // Lê o arquivo index.html
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' }); // Define o status de erro
-            res.end('Erro ao ler o arquivo'); // Envia a resposta de erro
-        } else {
-            const personalizedData = data.replace('{{name}}', name || 'Visitante'); // Substitui {{name}} pelo valor do parâmetro ou 'Visitante'
-            res.writeHead(200, { 'Content-Type': 'text/html' }); // Define o status de sucesso
-            res.end(personalizedData); // Envia a resposta com o conteúdo personalizado
-        }
-    });
+    if (!name) {
+        fs.readFile("index.html", "utf8", (err, data) => {
+            // Lê o arquivo index.html
+            res.writeHead(200, { "Content-Type": "text/html" }); // Define o status de erro
+            res.write(data); // Escreve o conteúdo do arquivo na resposta
+            return res.end("Erro ao ler o arquivo"); // Envia a resposta com o conteúdo do arquivo ou uma mensagem de erro
+        });
+    } else {
+        // Escreve o valor do parâmetro 'name' em um arquivo chamado 'arquivo.txt'
+        res.writeFile("arquivo.txt", name, function (err, data) {
+            // Redireciona para a página inicial após escrever o arquivo
+            res.writeHead(302, {
+                Location: "/",
+            });
+            return res.end(); // Envia a resposta para o cliente   
+        });
+    }
 });
+
+// Inicia o servidor e escuta na porta definida
+server.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`); // Exibe uma mensagem no console indicando que o servidor está rodando
+})
